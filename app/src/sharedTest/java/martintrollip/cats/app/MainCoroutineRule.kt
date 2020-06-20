e60599ad62ac009ba84948884d345a10e3215a22
Martin Trollip <martint@discovery.co.za>
2020-06-20 11:06:18 +0200

    [cats] Initial setup

---
+++ app/src/sharedTest/java/martintrollip/cats/app/MainCoroutineRule.kt
@@ -0,0 +1,31 @@
+package martintrollip.cats.app
+
+import android.util.EventLogTags
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+ * @author Martin Trollip
+ * @since 2020/04/14 09:32
+ */
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()):
+        TestWatcher(),
+        TestCoroutineScope by TestCoroutineScope(dispatcher) {
+    override fun starting(description: EventLogTags.Description?) {//@Before
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: EventLogTags.Description?) { //@After
+        super.finished(description)
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+    }
+}
